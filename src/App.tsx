81b6df5c6062f45da67da669eeae762955c60aaf
Marcin Ostaszewski <m.ostaszewski@azagroup.eu>
2021-11-24 20:13:25 +0100

    add components and styles to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Header } from './components/Header';
+import { AddTransaction } from './components/AddTransaction';
+import { TransactionsTable } from './components/TransactionsTable';
+import { Sum } from './components/Sum';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <AddTransaction />
+
+      <TransactionsTable />
+
+      <Sum />
+
     </div>
   );
 }
