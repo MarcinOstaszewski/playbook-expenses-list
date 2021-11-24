9738d070af89aa47b341f9cc9d163f02f1bf4908
Marcin Ostaszewski <m.ostaszewski@azagroup.eu>
2021-11-24 20:13:06 +0100

    create TransactionTable component

---
+++ src/components/TransactionsTable.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const TransactionsTable: React.FC = () => {
+    return <>
+        <table>
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Amount (PLN)</th>
+                    <th>Amount (EUR)</th>
+                    <th>Options</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </>
+}
