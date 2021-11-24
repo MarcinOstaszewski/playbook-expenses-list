26392d9f77c172b460ac8fc4d3fb5d1027d6baaa
Marcin Ostaszewski <m.ostaszewski@azagroup.eu>
2021-11-24 20:12:55 +0100

    create Sum component

---
+++ src/components/Sum.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Sum: React.FC = () => {
+    return <>
+        <div className="sum-display">
+            Sum of expenses: 333 PLN (111 EUR)
+        </div>
+    </>
+}
