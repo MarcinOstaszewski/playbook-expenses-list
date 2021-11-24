11b1d3163ebc51246aa7a67d481934ae60967122
Marcin Ostaszewski <m.ostaszewski@azagroup.eu>
2021-11-24 22:36:09 +0100

    minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Header } from './components/Header';
-import { AddTransaction } from './components/AddTransaction';
+import AddTransaction from './components/AddTransaction';
 import { TransactionsTable } from './components/TransactionsTable';
 import { Sum } from './components/Sum';
 
