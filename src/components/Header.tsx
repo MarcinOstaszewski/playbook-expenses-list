bae23eaf917fd64b58fd4c37d16bbb26b73a38da
Marcin Ostaszewski <m.ostaszewski@azagroup.eu>
2021-11-25 12:29:54 +0100

    add button to get current PLN to EUR rates from NBP

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,18 @@
-import * as React from 'react';
+import React from 'react';
 import { observer } from 'mobx-react';
 
 import store from '../store/store';
+import axios from 'axios';
 
 const Header: React.FC = () => {
+
+    const getCurrentRates = () => {
+        (async () => {
+            await axios.get('http://api.nbp.pl/api/exchangerates/rates/a/eur/')
+            .then( res => store.plnToEur = res.data.rates[0].mid);
+        })();
+    }
+
     const setPlnToEur = (value: number) => {
         return value > 0 ? value : 0.001;
     }
@@ -11,6 +20,7 @@ const Header: React.FC = () => {
     return <>
         <div className="header">
             <div className="title">List of expenses</div>
+            <div className="get-current-rates" onClick={getCurrentRates}>Get current rates</div>
             <div className="pln-to-eur">
                 <span className="pln-to-eur-label">1 EUR =</span>
                     <input
